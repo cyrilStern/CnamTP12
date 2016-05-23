f8dbb7935b422679a8829eac17baf93909360fe9
cyril <sterncyril@gmail.com>
2016-05-23 18:03:10 +0200

    start de l activité

--- app/src/main/java/fr/cyrilstern/cnam/cnamtp12/MainActivity.java
+++ app/src/main/java/fr/cyrilstern/cnam/cnamtp12/MainActivity.java
@@ -1,5 +1,7 @@
 package fr.cyrilstern.cnam.cnamtp12;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,4 +12,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    @Override
+    public void onResume() {
+        super.onResume();
+        IntentFilter intentFilter1 = new IntentFilter();
+        intentFilter1.addAction("android.nfc.actiob.NDEF_DISCOVERED");
+        intentFilter1.addCategory("android.intent.category.DEFAULT");
+        try {
+            intentFilter1.addDataType("text/palin");
+        } catch (IntentFilter.MalformedMimeTypeException e) {
+            e.printStackTrace();
+        }
+        IntentFilter intentFilter2 = new IntentFilter();
+        intentFilter2.addAction("android.nfc.action.TECH_DISCOVERED");
+        IntentFilter intentFilter3 = new IntentFilter();
+        intentFilter3.addAction("android.nfc.action.TAG_DISCOVERED");
+        intentFilter3.addCategory("android.intent.category.DEFAULT");
+
+
+    }
+
 }

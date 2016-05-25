3b229885cca0b160babed94f53994ddb150ea343
cyril <sterncyril@gmail.com>
2016-05-25 13:16:34 +0200

    ajout registration listener

--- app/src/main/java/fr/cyrilstern/cnam/cnamtp12/Main2ActivityDNS.java
+++ app/src/main/java/fr/cyrilstern/cnam/cnamtp12/Main2ActivityDNS.java
@@ -12,7 +12,7 @@ import android.widget.TextView;
 import java.io.IOException;
 import java.net.ServerSocket;
 
-public class Main2ActivityDNS extends AppCompatActivity {
+public class Main2ActivityDNS extends AppCompatActivity  implements NsdManager.RegistrationListener{
     private TextView tvServiceInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,18 +38,64 @@ public class Main2ActivityDNS extends AppCompatActivity {
 
         NsdManager nsdManager = (NsdManager) getSystemService(Context.NSD_SERVICE);
         try {
-            mServerSocket = new ServerSocket(0);
+            mServerSocket = new ServerSocket(0); //socket
         } catch (IOException e) {
             tvServiceInfo.setText("NSD Service Error:\n" + e.toString());
             e.printStackTrace();
         }
-        mLocalPort = mServerSocket.getLocalPort();
-        if(sdk< Build.VERSION.SDK_INT)
-        NsdServiceInfo serviceInfo = new NsdServiceInfo();
+        mLocalPort = mServerSocket.getLocalPort();  //register port find in local variable to give to the client whaen resolution
+        if(sdk>= Build.VERSION_CODES.KITKAT){
+        NsdServiceInfo serviceInfo = new NsdServiceInfo();  //service provide to ecveery wone
         serviceInfo.setServiceName(SERVICE_NAME);
         serviceInfo.setServiceType(SERVICE_TYPE);
         serviceInfo.setPort(mLocalPort);
+            nsdManager.registerService(serviceInfo, NsdManager.PROTOCOL_DNS_SD, this);
+        }
 
+
+
+    }
+
+
+
+    public void onServiceFound(NsdServiceInfo serviceInfo) {
+
+        serviceInfo.getServiceName();
+        serviceInfo.getServiceType();
+
+        NsdManager.ResolveListener mResolveListener = new NsdManager.ResolveListener() {
+        public void onResolveFailed(NsdServiceInfo serviceInfo, int errorCode) {
+
+        }
+        public void onServiceResolved(NsdServiceInfo serviceInfo) {
+
+            serviceInfo.getServiceName();
+            serviceInfo.getPort();
+            serviceInfo.getHost().getHostAddress();
+
+        }
+    };
+    mNsdManager.resolveService(serviceInfo, mResolveListener);
+}
+
+
+    @Override
+    public void onRegistrationFailed(NsdServiceInfo serviceInfo, int errorCode) {
+
+    }
+
+    @Override
+    public void onUnregistrationFailed(NsdServiceInfo serviceInfo, int errorCode) {
+
+    }
+
+    @Override
+    public void onServiceRegistered(NsdServiceInfo serviceInfo) {
+
+    }
+
+    @Override
+    public void onServiceUnregistered(NsdServiceInfo serviceInfo) {
 
     }
 }
